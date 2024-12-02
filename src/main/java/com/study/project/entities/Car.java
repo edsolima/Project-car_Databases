22bbb6ca4f1c810867ee1828c476f0e2ed0d3003
edsolima <ewerton.silva.o.lima@gmail.com>
2024-12-01 21:08:07 -0300

    Update Car entity with lombok annotations

--- src/main/java/com/study/project/entities/Car.java
+++ src/main/java/com/study/project/entities/Car.java
@@ -4,13 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
-import java.io.Serializable;
-import java.util.Objects;
+import lombok.*;
+import org.antlr.v4.runtime.misc.NotNull;
 
-@Getter
-@Setter
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 @Entity
 public class Car implements Serializable {
     private static final long serialVersionUID = 1L;
@@ -18,31 +19,11 @@ public class Car implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotNull
     private Integer year;
+    @NotNull
     private String model;
     private Float price;
 
-    public Car() {
-    }
-
-    public Car(Long id, Integer year, String model, Float price) {
-        this.id = id;
-        this.year = year;
-        this.model = model;
-        this.price = price;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Car car = (Car) o;
-        return Objects.equals(id, car.id) && Objects.equals(year, car.year) && Objects.equals(model, car.model) && Objects.equals(price, car.price);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, year, model, price);
-    }
 }
 
